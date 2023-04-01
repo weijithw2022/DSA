994a06a5315df6c05f9f441d12ede77ae5136473
Weijith Wimalasiri <110900801+weijithw2022@users.noreply.github.com>
2023-04-01 17:29:57 +0530

    Create hackerrank_closestNumbers_sol1.cpp

---
+++ hackerrank_closestNumbers_sol1.cpp
@@ -0,0 +1,42 @@
+int size = arr.size();
+    int min=0;
+    vector <int> vect1;
+    for(int i=0; i<size-1; i++){
+        for(int j=i+1; j<size; j++){
+            int temp =  abs(arr.at(i) - arr.at(j));
+            if(i==0 and j==1){
+                min = temp;
+                if(arr.at(j)>arr.at(i)){
+                    vect1.push_back(arr.at(i));
+                    vect1.push_back(arr.at(j));
+                }
+                else{
+                    vect1.push_back(arr.at(j));
+                    vect1.push_back(arr.at(i));
+                }
+            }
+            if(temp==min){
+                if(arr.at(j)>arr.at(i)){
+                    vect1.push_back(arr.at(i));
+                    vect1.push_back(arr.at(j));
+                }
+                else{
+                    vect1.push_back(arr.at(j));
+                    vect1.push_back(arr.at(i));
+                }
+            }
+            if(temp<min){
+                min = temp;
+                vect1.clear();
+                if(arr.at(j)>arr.at(i)){
+                    vect1.push_back(arr.at(i));
+                    vect1.push_back(arr.at(j));
+                }
+                else{
+                    vect1.push_back(arr.at(j));
+                    vect1.push_back(arr.at(i));
+                }
+            }
+        }
+    }
+    return vect1;
